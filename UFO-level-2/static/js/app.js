bb44a2a0239162f56eafeef06d615e4487f5543a
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-01-18 14:01:17 -0500

    selectAll

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -6,21 +6,49 @@ var tableData = data;
 var input = d3.selectAll("li");
 var button =d3.select("button")
 var tbody = d3.select("tbody");
+inputvalue ={}
 //var inputValue = input.property("value")
 input.on("change", function() {
-    var inputValue = d3.select(this);
-        console.log(`this ${inputValue}`);
-    var inputValue2 = d3.event.target.value;
-    console.log(`event target:${inputValue2}`);
-        
-          
-    var filter ={};
-    filter.datetime = d3.select("#datetime").property.value;
-   console.log(filter.datetime)
-    filter.city =d3.select("#city").property.value;
-    filter[state] =d3.select("#state").property.value;
-    filter[country] =d3.select("#country").property.value;
-    filter[shape] =d3.select("#shape").property.value;
+    var input = d3.event.target.value;
+    // console.log(` This : ${input}`);
+   var datevalue= d3.select("#datetime").property('value');
+    
+        //console.log(`DAtetiem: ${datevalue}`);
+    if (datevalue !== " ") {
+        inputvalue["datetime"] = datevalue;
+        // console.log(`date:${datevalue}`);
+         };
+    var cityvalue = d3.select("#city").property('value');
+    // console.log(` city: ${cityvalue}`);
+    if (cityvalue !== " ") {
+        inputvalue["city"] = cityvalue};
+       
+    var statevalue = d3.select("#state").property('value');
+    // console.log(` state: ${statevalue}`);
+    if (statevalue !==" ") {
+        inputvalue["state"] = statevalue};
+
+    var countryvalue = d3.select("#country").property('value');
+    // console.log(` country: ${countryvalue}`);
+    if (countryvalue !== " ") {
+        inputvalue["country"] = countryvalue};
+    console.log (`filter: ${filter}`)
+
+    var shapevalue = d3.select("#shape").property('value');
+    // console.log(` shape: ${shapevalue}`);
+    if (shapevalue !== " ") {
+        inputvalue["shape"] = shapevalue};
+    
+    // console.log (`filter: ${filter}`)
+
+    // console.log(`date: ${filter.datetime}`);
+    // console.log(`state: ${filter.state}`);
+    // console.log(`shape: ${filter.shape}`);
+
+    Object.entries(inputvalue([key,value]){ 
+        filter row[key]===value
+    });
+
     var filterdata = tableData.filter( UFO => UFO.datetime ==inputValue);
     
     button.on ("click", function() {
@@ -34,37 +62,3 @@ input.on("change", function() {
             row.append("td").text(value);
         });
     });
- });
-});
-// // setting variables
-// var button = d3.select("#filter-btn")
-
-// button.on("click", function() {
-
-//   // date 
-//   var inputdate = d3.select("#datetime");
-//   var datevalue  = inputdate.property("value");
-//   console.log(datevalue);
-
-//   // city
-//   var inputcity = d3.select("#city");
-//   var cityvalue  = inputcity.property("value");
-//   console.log(cityvalue);
-  
-// // state
-// var inputstate = d3.select("#state");
-// var statevalue = inputstate.property("value");
-// console.log(statevalue);
-
-// // country
-// var inputcountry = d3.select ("#country");
-// var countryvalue = inputcountry.property("value");
-// console.log(countryvalue);
-
-// // shape
-// var inputshape = d3.select ("#shape");
-// var shapevalue = inputshape.property("value");
-// console.log(shapevalue);
-  
-
-// });
