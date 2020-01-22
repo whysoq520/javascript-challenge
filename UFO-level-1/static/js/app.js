4e6671b5c1a7dbe9e1d4ebdb83e7b05f5ea3ecc4
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-01-22 10:50:30 -0500

    update level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,22 +5,34 @@ var tableData = data;
 var input = d3.select("#datetime");
 var button =d3.select("button")
 var tbody = d3.select("tbody");
-//var inputValue = input.property("value")
-input.on("change", function() {
-    var inputValue = d3.event.target.value;
-    console.log(inputValue);
-    var filterdata = tableData.filter( UFO => UFO.datetime ==inputValue);
-    
-    button.on ("click", function() {
-       
-    filterdata.forEach((data) =>{
+
+// build funtion to biuld the table
+
+function builtTable(data) {
+    tbody.html("");
+    data.forEach((datavalue) =>{
         var row = tbody.append("tr");
         row.text(" ")
         
         //Object.values(data).forEach(value =>console.log(value));
-        Object.values(data).forEach(value =>{
+        Object.values(datavalue).forEach(value =>{
             row.append("td").text(value);
         });
     });
+    };
+
+builtTable(tableData);
+
+
+//var inputValue = input.property("value")
+input.on("change", function() {
+    var inputValue = d3.event.target.value;
+   // console.log(inputValue);
+    var filterdata = tableData.filter( UFO => UFO.datetime ==inputValue);
+    console.log(filterdata);
+    
+    button.on ("click", function() {
+    builtTable(filterdata)  
+    
  });
 });
