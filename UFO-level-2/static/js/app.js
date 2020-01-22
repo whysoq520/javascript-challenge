41507e5f598bb5d058c1754f767e12309b8964b0
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-01-22 13:43:09 -0500

    final

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -26,12 +26,14 @@ function filtertable(){
         data1 = data1.filter(row =>row[key]===value);
     });
     builtTable(data1);
-}
+};
 
 function updateData () {
-    var initial = d3.select(this).selectAll("input");
+    var initial = d3.select(this).select("input");
     var initialValue = initial.property("value");
     var initialID =initial.attr("id");
+    console.log(initialID);
+    console.log(initialValue);
 
     if(initialValue){
         filterdata[initialID] = initialValue;
@@ -41,54 +43,7 @@ function updateData () {
     }
     filtertable();
 };
-d3.select(".filter").on("change", updateData);
+d3.selectAll(".filter").on("change", updateData);
 
 
 builtTable(tableData);
-
-// var input = d3.selectAll("li");
-// var button =d3.select("button")
-// var tbody = d3.select("tbody");
-// inputvalue ={}
-// //var inputValue = input.property("value")
-// input.on("change", function() {
-//     var input = d3.event.target.value;
-//     // console.log(` This : ${input}`);
-//    var datevalue= d3.select("#datetime").property('value');
-    
-//         //console.log(`DAtetiem: ${datevalue}`);
-//     if (datevalue != " ") {
-//         inputvalue["datetime"] = datevalue;
-//         // console.log(`date:${datevalue}`);
-//          };
-//     var cityvalue = d3.select("#city").property('value');
-//     // console.log(` city: ${cityvalue}`);
-//     if (cityvalue !=" ") {
-//         inputvalue["city"] = cityvalue};
-       
-//     var statevalue = d3.select("#state").property('value');
-//     // console.log(` state: ${statevalue}`);
-//     if (statevalue !=" ") {
-//         inputvalue["state"] = statevalue};
-
-//     var countryvalue = d3.select("#country").property('value');
-//     // console.log(` country: ${countryvalue}`);
-//     if (countryvalue != " ") {
-//         inputvalue["country"] = countryvalue};
-    
-
-//     var shapevalue = d3.select("#shape").property('value');
-//     // console.log(` shape: ${shapevalue}`);
-//     if (shapevalue != " ") {
-//         inputvalue["shape"] = shapevalue};
-
-//         //console.log (`inputvalue: ${inputvalue}`);
-
-//     // console.log(`date: ${filter.datetime}`);
-//     // console.log(`state: ${filter.state}`);
-//     // console.log(`shape: ${filter.shape}`);
-//     Object.entries(inputvalue).forEach(([key, value]) => console.log(`Key: ${key} ; Value: ${value}`));
-//    //tableData.forEach(data =>console.log(`datetime:${data.datetime}, 'shape': ${data.shape}`));
- 
-//     //var filterdata = tableData.filter( UFO => UFO.datetime ==inputValue);
-// });
